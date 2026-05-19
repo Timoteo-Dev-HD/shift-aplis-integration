ebbbefe9a39886e34d5f21d0d7ddd6c7d87c4c3e
Timoteo <timoteo@hdinfo-ma.com>
2026-05-19 15:08:28 -0300

    feat: migrates e adição no src/__init__.py do projeto main.

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 from .settings.config import Config
-from .settings.extensions import db, migrate
+from .settings.extensions import db, migrate, auth_basic
 
 def create_app():
     app = Flask(__name__)
@@ -8,10 +8,20 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
+    auth = auth_basic()
 
     try:
         from .routes import register_routes
         register_routes(app)
+        
+        
+        # Import models
+        from src.models.aplis_request_model import AplisRequest
+        from src.models.shift_order_model import ShiftOrder
+        from src.models.audit_log_model import AuditLog
+        from src.models.webhook_event_model import WebhookEvent
+        from src.models.integration_log_model import IntegrationLog
+        
     except Exception:
         pass
 
