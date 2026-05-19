c199e2ece40c97f2759d9047ad6ee8a2c8521d86
Timoteo <timoteo@hdinfo-ma.com>
2026-05-19 15:05:51 -0300

    config: lib flask_httpauth, extensions.py

--- backend/src/settings/extensions.py
+++ backend/src/settings/extensions.py
@@ -1,5 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_httpauth import HTTPBasicAuth
 
 db = SQLAlchemy()
 migrate = Migrate()
+auth_basic = HTTPBasicAuth()
